e6988886e491d6cff86fe0adc0ec64aafb08418a
salama <a541996salamh@gmail.com>
2022-01-13 15:32:19 +0300

    add dark mood

--- js/main.js
+++ js/main.js
@@ -1,4 +1,64 @@
-function myFunction() {
-  var element = document.getElementById("navmobile");
-  element.classList.toggle("navmobile1");
+var icon=document.getElementById("icon");
+icon.onclick=function()
+{
+    document.body.classList.toggle("dark");
+    if( document.body.classList.contains("dark"))
+    {icon.src="licon.png";}
+
+else{icon.src="dicon.png";}
+
 }
+
+
+/************valdition************* */
+
+function myFunction()
+{
+  let comment=document.getElementById('comment').value;
+let uname=document.getElementById('username').value;
+let email=document.getElementById('email').value;
+
+
+  if(uname.value=="")
+  {
+    document.getElementById("error1").innerHTML = "Input not valid";
+  }
+
+}
+
+
+
+
+
+
+/************* menu ************ */
+
+
+
+function togglemobile(menu)
+ {
+	menu.classList.toggle('open');
+}
+
+
+
+
+
+/***************   effected *************** */
+
+function effectedimg(x) {
+  x.style.border="#ffffff 4px solid";
+
+
+}
+
+function normalimg(x) {
+  x.style.border="#ffffff 40px solid";
+ 
+ 
+}
+
+
+
+
+/*************** Dark mood ************** */
